3ac1c68413e798d26974ba4e21d14b81d148bd51
jesusnboone <jesusnboone@gmail.com>
2020-06-12 05:10:01 -0500

    messing with onclick events, progress save

--- assets/javascript.js
+++ assets/javascript.js
@@ -11,14 +11,31 @@ var questionAnswerTwo = document.getElementById("q-answer-two");
 var questionAnswerThree = document.getElementById("q-answer-three");
 var questionAnswerFour = document.getElementById("q-answer-four");
 
+var AnswerOneTrue = ""
+var AnswerTwoTrue = ""
+var AnswerThreeTrue = ""
+var AnswerFourTrue = ""
+
+questionAnswerOne.addEventListener("click", function(){
+    AnswerOneTrue = "true"
+});
+questionAnswerTwo.addEventListener("click", function(){
+    AnswerTwoTrue = "true"
+});
+questionAnswerThree.addEventListener("click", function(){
+    AnswerThreeTrue = "true"
+});
+questionAnswerFour.addEventListener("click", function(){
+    AnswerFourTrue = "true"
+});
+
 var questions = [
-    { q: 'A very useful tool used during development an debugging for printing content to the debugger is:', a1: '1. Javascript', a2: '2. terminal/bash', a3: '3.'},
-    { q: 'Commonly used data typed DO NOT include:', a: '' },
-    { q: 'The condition in an if/else statement is enclosed with ____', a: 't' },
-    { q: 'Arrays in Javascript can be used to store', a: 'f' },
+    { q: 'A very useful tool used during development and debugging for printing content to the debugger is:', a1: '1. Javascript', a2: '2. terminal/bash', a3: '3. for loops', a4: '4. console.log'},
+    { q: 'Commonly used data typed DO NOT include:', a1: 'strings', a2: 'booleans', a3: 'alerts', a4: 'numbers' },
+    { q: 'The condition in an if/else statement is enclosed with ____', a1: 'quotes', a2: 'curly brackets', a3: 'parenthesis', a4: 'square brackets'},
+    { q: 'Arrays in Javascript can be used to store', a1: 'numbers and strings', a2: 'other arrays', a3: 'booleans', a4: 'all of the above'},
     { q: 'String values must be inclosed within ____ when being assigned to variables', a1: 'commas', a2: 'curly brackets', a3: 'quotes', a4: 'parenthesis'}
 ];
-
 var score = 0;
 
 question.style.display = "none";
@@ -35,35 +52,26 @@ function startQuiz() {
     question.style.display = "block";
     answers.style.display = "block";
 
-    for (var i = 0; i < questions.length; i++) {
-        question.textContent = (questions[i].q)
-        questionAnswerOne.textContent = (questions[i].a1)
-        questionAnswerTwo.textContent = (questions[i].a2)
-        questionAnswerThree.textContent = (question[i].a3)
-        questionAnswerFour.textContent = (question[i].a4)
+    question.textContent = (questions[0].q)
+    questionAnswerOne.textContent = (questions[0].a1)
+    questionAnswerTwo.textContent = (questions[0].a2)
+    questionAnswerThree.textContent = (questions[0].a3)
+    questionAnswerFour.textContent = (questions[0].a4)
+
+    questionAnswerOne.addEventListener("click", function(){
+        AnswerOneTrue = "true";
+    });
+    questionAnswerTwo.addEventListener("click", function(){
+        AnswerTwoTrue = "true";
+    });
+    questionAnswerThree.addEventListener("click", function(){
+        AnswerThreeTrue = "true";
+    });
+    questionAnswerFour.addEventListener("click", function(){
+        AnswerFourTrue = "true";
+    });
+
+    if (questions[0].q && AnswerFourTrue) {
+        console.log("awesome");
     }
 }
-
-
-
-
-//answer
-// Display current question to user and ask OK/Cancel
-//var answer = (questions[i].q);}
-// Loop over every question object
-//for (var i = 0; i < questions.length; i++) {
-//question.style.textContent = ("questions[i].q")
-// Display current question to user and ask OK/Cancel
-//var answer = (questions[i].q);}
-// Compare answers
-   // if (
-        //(answer === true && questions[i].a === 't') ||
-        //(answer === false && questions[i].a === 'f')
-         // ) {
-            // Increase score
-          //  score++;
-          //  alert('Correct!');
-          //} else {
-         //   alert('Wrong!');
-          //}
-       // }
