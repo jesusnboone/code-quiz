c26b4abc161317a6b209d9b1fa1ed815298aebcf
jesusnboone <jesusnboone@gmail.com>
2020-06-12 06:17:39 -0500

    customized questions for on-click events, modified timer

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,6 @@
 var title = document.getElementById("title");
 var start = document.getElementById("start");
+var timer = document.getElementById("timer");
 var instruction = document.getElementById("instruction");
 var question = document.getElementById("question");
 var answers = document.getElementById("answers");
@@ -11,24 +12,101 @@ var questionAnswerTwo = document.getElementById("q-answer-two");
 var questionAnswerThree = document.getElementById("q-answer-three");
 var questionAnswerFour = document.getElementById("q-answer-four");
 
-var AnswerOneTrue = ""
-var AnswerTwoTrue = ""
-var AnswerThreeTrue = ""
-var AnswerFourTrue = ""
+function questionTwo () {
+    question.textContent = (questions[1].q)
+    questionAnswerOne.textContent = (questions[1].a1)
+    questionAnswerTwo.textContent = (questions[1].a2)
+    questionAnswerThree.textContent = (questions[1].a3)
+    questionAnswerFour.textContent = (questions[1].a4)
 
-questionAnswerOne.addEventListener("click", function(){
-    AnswerOneTrue = "true"
-});
-questionAnswerTwo.addEventListener("click", function(){
-    AnswerTwoTrue = "true"
-});
-questionAnswerThree.addEventListener("click", function(){
-    AnswerThreeTrue = "true"
-});
-questionAnswerFour.addEventListener("click", function(){
-    AnswerFourTrue = "true"
-});
+    questionAnswerOne.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerTwo.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerThree.addEventListener("click", function(){
+       questionThree()
+    });
+    questionAnswerFour.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+}
 
+function questionThree () {
+    question.textContent = (questions[2].q)
+    questionAnswerOne.textContent = (questions[2].a1)
+    questionAnswerTwo.textContent = (questions[2].a2)
+    questionAnswerThree.textContent = (questions[2].a3)
+    questionAnswerFour.textContent = (questions[2].a4)
+
+    questionAnswerOne.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerTwo.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerThree.addEventListener("click", function(){
+       questionFour()
+    });
+    questionAnswerFour.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+}
+
+function questionFour () {
+    question.textContent = (questions[3].q)
+    questionAnswerOne.textContent = (questions[3].a1)
+    questionAnswerTwo.textContent = (questions[3].a2)
+    questionAnswerThree.textContent = (questions[3].a3)
+    questionAnswerFour.textContent = (questions[3].a4)
+
+    questionAnswerOne.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerTwo.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerThree.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerFour.addEventListener("click", function(){
+        questionFive();
+    });
+}
+
+function questionFive () {
+    question.textContent = (questions[4].q)
+    questionAnswerOne.textContent = (questions[4].a1)
+    questionAnswerTwo.textContent = (questions[4].a2)
+    questionAnswerThree.textContent = (questions[4].a3)
+    questionAnswerFour.textContent = (questions[4].a4)
+
+    questionAnswerOne.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerTwo.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+    questionAnswerThree.addEventListener("click", function(){
+        highScore ();
+    });
+    questionAnswerFour.addEventListener("click", function(){
+        if (time>10){
+        time=time-10}
+    });
+}
 var questions = [
     { q: 'A very useful tool used during development and debugging for printing content to the debugger is:', a1: '1. Javascript', a2: '2. terminal/bash', a3: '3. for loops', a4: '4. console.log'},
     { q: 'Commonly used data typed DO NOT include:', a1: 'strings', a2: 'booleans', a3: 'alerts', a4: 'numbers' },
@@ -37,7 +115,9 @@ var questions = [
     { q: 'String values must be inclosed within ____ when being assigned to variables', a1: 'commas', a2: 'curly brackets', a3: 'quotes', a4: 'parenthesis'}
 ];
 var score = 0;
+var time = 75;
 
+timer.textContent = ("Time:" + time);
 question.style.display = "none";
 answers.style.display = "none";
 end.style.display = "none";
@@ -45,6 +125,20 @@ final.style.display = "none";
 form.style.display = "none";
 
 function startQuiz() {
+    var time = 74;
+
+    var timeInterval = setInterval(function () {
+      timer.textContent = ("Time:" + time);
+      time--;
+      if (time===0 || time<0) {
+        timer.textContent = ("Time:0")
+        clearInterval(timeInterval);
+        endGame()
+
+      }
+    
+    }, 1000);
+
     title.style.display = "none";
     start.style.display = "none";
     instruction.style.display = "none";
@@ -57,21 +151,22 @@ function startQuiz() {
     questionAnswerTwo.textContent = (questions[0].a2)
     questionAnswerThree.textContent = (questions[0].a3)
     questionAnswerFour.textContent = (questions[0].a4)
-
+    
+    
     questionAnswerOne.addEventListener("click", function(){
-        AnswerOneTrue = "true";
+        if (time>10){
+        time=time-10}
     });
     questionAnswerTwo.addEventListener("click", function(){
-        AnswerTwoTrue = "true";
+        if (time>10){
+        time=time-10}
     });
     questionAnswerThree.addEventListener("click", function(){
-        AnswerThreeTrue = "true";
+        if (time>10){
+            time=time-10}
     });
     questionAnswerFour.addEventListener("click", function(){
-        AnswerFourTrue = "true";
+        questionTwo()
     });
-
-    if (questions[0].q && AnswerFourTrue) {
-        console.log("awesome");
-    }
 }
+
