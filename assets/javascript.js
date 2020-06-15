8fbae0f00d14c04266306701ec054b3ff7dc7373
jesusnboone <jesusnboone@gmail.com>
2020-06-14 22:32:35 -0500

    added styling, timer, and DRY

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,16 +1,36 @@
-var title = document.getElementById("title");
-var start = document.getElementById("start");
-var timer = document.getElementById("timer");
-var instruction = document.getElementById("instruction");
-var question = document.getElementById("question");
-var answers = document.getElementById("answers");
-var end = document.getElementById("end");
-var final = document.getElementById("final");
-var form = document.getElementById("form");
-var questionAnswerOne = document.getElementById("q-answer-one");
-var questionAnswerTwo = document.getElementById("q-answer-two");
-var questionAnswerThree = document.getElementById("q-answer-three");
-var questionAnswerFour = document.getElementById("q-answer-four");
+var title = document.querySelector("#title");
+var start = document.querySelector("#start");
+var timer = document.querySelector("#timer");
+var instruction = document.querySelector("#instruction");
+var question = document.querySelector("#question");
+var answers = document.querySelector("#answers");
+var end = document.querySelector("#end");
+var final = document.querySelector("#final");
+var questionAnswerOne = document.querySelector("#q-answer-one");
+var questionAnswerTwo = document.querySelector("#q-answer-two");
+var questionAnswerThree = document.querySelector("#q-answer-three");
+var questionAnswerFour = document.querySelector("#q-answer-four");
+var submitScore = document.querySelector("#submit-score");
+var nameSpan = document.querySelector("#name-span");
+var nameInput = document.querySelector("#name-input");
+var highScorePage = document.querySelector("#high-score-page");
+var msgDiv = document.querySelector("#msg");
+var score = 0;
+var time = 75;
+
+timer.textContent = ("Time:" + time);
+question.style.display = "none";
+answers.style.display = "none";
+final.style.display = "none";
+highScorePage.style.display = "none";
+
+var questions = [
+    { q: 'A very useful tool used during development and debugging for printing content to the debugger is:', a1: '1. Javascript', a2: '2. terminal/bash', a3: '3. for loops', a4: '4. console.log'},
+    { q: 'Commonly used data typed DO NOT include:', a1: 'strings', a2: 'booleans', a3: 'alerts', a4: 'numbers' },
+    { q: 'The condition in an if/else statement is enclosed with ____', a1: 'quotes', a2: 'curly brackets', a3: 'parenthesis', a4: 'square brackets'},
+    { q: 'Arrays in Javascript can be used to store', a1: 'numbers and strings', a2: 'other arrays', a3: 'booleans', a4: 'all of the above'},
+    { q: 'String values must be inclosed within ____ when being assigned to variables', a1: 'commas', a2: 'curly brackets', a3: 'quotes', a4: 'parenthesis'}
+];
 
 function questionTwo () {
 
@@ -21,18 +41,18 @@ function questionTwo () {
     questionAnswerFour.textContent = (questions[1].a4)
 
     questionAnswerOne.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionThree()
     });
     questionAnswerTwo.addEventListener("click", function(){
-            time=time-10
+            time-=10;
             questionThree()
     });
     questionAnswerThree.addEventListener("click", function(){
        questionThree()
     });
     questionAnswerFour.addEventListener("click", function(){
-            time=time-10
+            time-=10;
             questionThree()
     });
 }
@@ -45,18 +65,18 @@ function questionThree () {
     questionAnswerFour.textContent = (questions[2].a4)
 
     questionAnswerOne.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionFour()
     });
     questionAnswerTwo.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionFour()
     });
     questionAnswerThree.addEventListener("click", function(){
        questionFour()
     });
     questionAnswerFour.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionFour()
     });
 }
@@ -69,15 +89,15 @@ function questionFour () {
     questionAnswerFour.textContent = (questions[3].a4)
 
     questionAnswerOne.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionFive();
     });
     questionAnswerTwo.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionFive();
     });
     questionAnswerThree.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionFive();
     });
     questionAnswerFour.addEventListener("click", function(){
@@ -91,39 +111,23 @@ function questionFive () {
     questionAnswerTwo.textContent = (questions[4].a2)
     questionAnswerThree.textContent = (questions[4].a3)
     questionAnswerFour.textContent = (questions[4].a4)
-
+    
     questionAnswerOne.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         highScore ();
     });
     questionAnswerTwo.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         highScore ();
     });
     questionAnswerThree.addEventListener("click", function(){
         highScore ();
     });
     questionAnswerFour.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         highScore ();
     });
 }
-var questions = [
-    { q: 'A very useful tool used during development and debugging for printing content to the debugger is:', a1: '1. Javascript', a2: '2. terminal/bash', a3: '3. for loops', a4: '4. console.log'},
-    { q: 'Commonly used data typed DO NOT include:', a1: 'strings', a2: 'booleans', a3: 'alerts', a4: 'numbers' },
-    { q: 'The condition in an if/else statement is enclosed with ____', a1: 'quotes', a2: 'curly brackets', a3: 'parenthesis', a4: 'square brackets'},
-    { q: 'Arrays in Javascript can be used to store', a1: 'numbers and strings', a2: 'other arrays', a3: 'booleans', a4: 'all of the above'},
-    { q: 'String values must be inclosed within ____ when being assigned to variables', a1: 'commas', a2: 'curly brackets', a3: 'quotes', a4: 'parenthesis'}
-];
-var score = 0;
-var time = 75;
-
-timer.textContent = ("Time:" + time);
-question.style.display = "none";
-answers.style.display = "none";
-end.style.display = "none";
-final.style.display = "none";
-form.style.display = "none";
 
 function startQuiz() {
     var time = 74;
@@ -154,15 +158,15 @@ function startQuiz() {
     questionAnswerFour.textContent = (questions[0].a4)
     
     questionAnswerOne.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionTwo()
     });
     questionAnswerTwo.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionTwo()
     });
     questionAnswerThree.addEventListener("click", function(){
-        time=time-10
+        time-=10;
         questionTwo()
     });
     questionAnswerFour.addEventListener("click", function(){
@@ -173,8 +177,39 @@ function startQuiz() {
 function highScore () {
     question.style.display = "none";
     answers.style.display = "none";
-    end.style.display = "block";
     final.style.dispaly = "block";
     final.textContent = ("Your final score is" + timer);
-    form.style.display = "block";
+    highScorePage.style.display = "block";
 }
+
+
+function getHighScore() {
+    var name = localStorage.getItem("name");
+    if (name === null) {
+      return;
+    }
+  
+    nameSpan.textContent = name;
+  }
+  
+  getHighScore();
+  
+  function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute("class", type);
+}
+  
+  
+submitScore.addEventListener("click", function (event) {
+    event.preventDefault();
+  
+    var name = document.querySelector("#name").value;
+  
+    if (name === "") {
+      displayMessage("Please enter your name!");
+    } else {
+  
+      localStorage.setItem("name", name + ":" + time);
+      getHighScore();
+    }}
+);
